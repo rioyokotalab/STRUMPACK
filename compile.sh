b08c0012d697ee62887d027b008a6b27b2bb3929
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2022-05-12 11:39:50 +0900

    update compile with LP64

--- compile.sh
+++ compile.sh
@@ -1,16 +1,15 @@
 
 module purge
-module load gcc/11.2.0 openmpi/4.1.3 intel-mkl/2022.0.0 cmake/3.22.3
+module load gcc/11.2.0 intel-mpi intel-mkl/2022.0.0 cmake/3.22.3
 
 rm -rf build
 mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/gitrepos/STRUMPACK/build \
-      -DTPL_BLAS_LIBRARIES="-L${MKLROOT}/lib/intel64 -Wl,--no-as-needed -lmkl_intel_ilp64 -lmkl_gnu_thread -lmkl_core -lgomp -lpthread -lm -ldl" \
-      -DTPL_LAPACK_LIBRARIES="-L${MKLROOT}/lib/intel64 -Wl,--no-as-needed -lmkl_intel_ilp64 -lmkl_gnu_thread -lmkl_core -lgomp -lpthread -lm -ldl" \
-      -DTPL_SCALAPACK_LIBRARIES="-L${MKLROOT}/lib/intel64 -lmkl_scalapack_ilp64 -Wl,--no-as-needed -lmkl_intel_ilp64 -lmkl_gnu_thread -lmkl_core -lmkl_blacs_intelmpi_ilp64 -lgomp -lpthread -lm -ldl"
+      -DTPL_BLAS_LIBRARIES="-L${MKLROOT}/lib/intel64 -Wl,--no-as-needed -lmkl_intel_lp64 -lmkl_gnu_thread -lmkl_core -lgomp -lpthread -lm -ldl" \
+      -DTPL_LAPACK_LIBRARIES="-L${MKLROOT}/lib/intel64 -Wl,--no-as-needed -lmkl_intel_lp64 -lmkl_gnu_thread -lmkl_core -lgomp -lpthread -lm -ldl" \
+      -DTPL_SCALAPACK_LIBRARIES=" -L${MKLROOT}/lib/intel64 -lmkl_scalapack_lp64 -Wl,--no-as-needed -lmkl_intel_lp64 -lmkl_gnu_thread -lmkl_core -lmkl_blacs_intelmpi_lp64 -lgomp -lpthread -lm -ldl " \
+      -DCMAKE_BUILD_TYPE=Debug
 
 make -j install
-make -j tests
-make -j test
-
+make -j examples
