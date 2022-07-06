927b0f96a4b1612b9a13cd1d2391d47b2139ce78
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2022-07-06 18:47:58 +0900

    update compilers

--- compile_fugaku.sh
+++ compile_fugaku.sh
@@ -8,7 +8,9 @@ ROOT_FOLDER=$PWD
 mkdir build
 cd build
 export LDFLAGS="$(pkg-config --libs starsh)"
-cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/gitrepos/STRUMPACK/build \
+export CXX=mpiFCC
+export FX=mpifrt
+CXX=mpiFCC FC=mpifrt CC=mpifcc cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/gitrepos/STRUMPACK/build \
 	-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="$(pkg-config --cflags starsh)" -DCMAKE_EXE_LINKER_FLAGS="$(pkg-config --libs starsh)" \
       -DCMAKE_INSTALL_RPATH="/home/acb10922qh/gitrepos/lorapo/stars-h-rio/build/installdir/lib"
 
