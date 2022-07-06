9996366ba866a1f8bde33e81563a10dda5597814
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2022-07-06 16:49:39 +0900

    udpate fugaku compile

---
+++ compile_fugaku.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+. /vol0004/apps/oss/spack/share/spack/setup-env.sh
+
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/home/hp190122/u01594/gitrepos/lorapo/stars-h-rio/build/installdir/lib/pkgconfig
+ROOT_FOLDER=$PWD
+
+mkdir build
+cd build
+export LDFLAGS="$(pkg-config --libs starsh)"
+cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/gitrepos/STRUMPACK/build \
+	-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="$(pkg-config --cflags starsh)" -DCMAKE_EXE_LINKER_FLAGS="$(pkg-config --libs starsh)" \
+      -DCMAKE_INSTALL_RPATH="/home/acb10922qh/gitrepos/lorapo/stars-h-rio/build/installdir/lib"
+
